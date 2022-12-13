876263bbc393c4251785c73deb6d270e800dc6c1
arain58 <119989933+arain58@users.noreply.github.com>
2022-12-13 13:07:12 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,11 +29,11 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
-global.ytname = "" // yt chanel name
-global.socialm = "GitHub: prabhasha2006" // github or insta name
-global.location = "India, Mizoram, Aizawl" // location
+global.vcardowner = ['+923137103835'] // owner number
+global.ownername = "😎𝑩𝑨𝑫 𝑩𝑶𝒀 ❯❯" // owner name
+global.ytname = "https://www.youtube.com/@badboy5895" // yt chanel name
+global.socialm = "GitHub: https://github.com/arain58" // github or insta name
+global.location = "Pakistan, chichawatni, Chak 58/12-L" // location
 
 //bot body 
 global.alivepic = `https://i.ibb.co/cyyCjN7/20220803-081426.png` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
